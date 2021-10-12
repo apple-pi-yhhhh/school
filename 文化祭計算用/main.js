2e318c7b04b2800b8ccb39a6e0169f425605bd8f
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2021-10-12 14:25:59 +0900

    Update main.js

--- 文化祭計算用/main.js
+++ 文化祭計算用/main.js
@@ -1,4 +1,55 @@
-let time;
-let member;
-let mass_k;
-let mass_s;
+let time;			//時間
+
+let member;		//人数
+
+let mass_k;		//マス-教室
+let mass_s;		//マス-視聴会
+
+let dice;			//サイコロ平均
+
+let Result;		//結果
+
+//「num」に計算したいやつ打ち込む
+switch (num){
+	case "time" :
+		break;
+	
+	case "member" :
+		break;
+	
+	case "mass_k" :
+		break;
+	
+	case "mass_s" :
+		break;
+	
+	case "dice" :
+		break;
+	
+	default :
+		document.write("<h2>"+Result+"</h2>")
+}
+//時間
+if (num = "_time_"){
+	
+}
+
+//人数
+if (num = "_member_"){
+	
+}
+
+//マス-教室
+if (num = "_mass_k_"){
+	
+}
+
+//マス-視聴会
+if (num = "_mass_s_"){
+	
+}
+
+//サイコロ平均
+if (num = "_dice_"){
+	
+}
